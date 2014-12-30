0bc575beb532483dcc7229c098cd33ea7921cf67
zhangyaochun <zhangyaochun@wandoujia.com>
2014-12-30 18:11:38 +0800

    [command line] commit auto by @wd-deploy on 2014-12-30 06:11:38

--- pre-build.js
+++ pre-build.js
@@ -2,20 +2,22 @@ var superagent = require('superagent');
 
 superagent.get('https://api.github.com/orgs/wandoulabs/repos?type=public')
     .end(function (err, res) {
+
         console.log('/**');
         console.log(' * This file automatically generated from `pre-build.js`.');
-        console.log(' * Do not manually edit.');
+        console.log(' * Please do not manually edit.');
         console.log(' */');
         console.log();
 
         var repos = {},
             i = 0,
-            length = res.length;
+            data = res.body,
+            length = data.length;
 
         for (; i < length; i++) {
             //now i use html_url
             //TODO
-            repos[res[i].name] = res[i]['html_url'];
+            repos[data[i].name] = data[i]['html_url'];
         }
 
         console.log('module.exports = %s;', JSON.stringify(repos, null, 2));
